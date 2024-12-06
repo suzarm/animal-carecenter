41e754d0c313473fa3596eb505c7c456e90a8871
suzarm <suzarm8883@gmail.com>
2024-12-06 10:37:33 +0400

    Finalized Navbar styling

--- animal-carecenter/src/components/Navbar.js
+++ animal-carecenter/src/components/Navbar.js
@@ -1,14 +1,15 @@
+import './Navbar.css';
 
 function Navbar(){
 return(
     <div className='navbar'>
-        <h1>Power Grid Homepage</h1>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Careers</li>
-            <li>Contact Us</li>
+        <h1 className='title'>Power Grid Homepage</h1>
+        <ul className='navbar-list'>
+            <li className='navbar-item'>Home</li>
+            <li className='navbar-item'>About</li>
+            <li className='navbar-item'>Blog</li>
+            <li className='navbar-item'>Careers</li>
+            <li className='navbar-item'>Contact Us</li>
         </ul>
     </div>
     )
